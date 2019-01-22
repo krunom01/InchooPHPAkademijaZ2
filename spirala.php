35c475ecc9d287846dc4cb72c7b3702e7da28e8a
krunom01 <krunom92@gmail.com>
2019-01-22 03:16:41 +0100

    found solution with few youtube clips and stackoverflow

--- spirala.php
+++ spirala.php
@@ -1,20 +1,36 @@
 <?php
+/*
+online tutorials:
+https://www.youtube.com/watch?v=TmweBVEL0I0
+https://www.youtube.com/watch?v=siKFOI8PNKM
+https://www.youtube.com/watch?v=efqZRnstLz4
+https://www.geeksforgeeks.org/print-a-given-matrix-in-spiral-form
+*/
+$n =16;
+$count = 1;
 
-$row = 4;
-$col =4;
-
-
-$data = array();
-
-//fill the first row
-    for($i=0;$i<$row;$i++){
-        $kolone = array();
-        for($j=0;$j<$col;$j++){
-            $kolone[]=0;
-        }
-        $data[]=$kolone;
+for($i=0;$i<$n;$i++){
+    for($j = $i; $j<=$n-1-$i;$j++){
+        $array[$i][$j]=$count++;
     }
-
-    print_r($data);
+    for($j=$i+1;$j<=$n-1-$i;$j++){
+        $array[$j][$n-1-$i]=$count++;
+    }
+    for($j=$n-2-$i;$j>=$i;$j--){
+        $array[$n-1-$i][$j]=$count++;
+    }
+    for($j=$n-2-$i;$j>$i;$j--){
+        $array[$j][$i]=$count++;
+    }
+}
+echo "<table>";
+for($i=0;$i<$n;$i++){
+    echo "<tr>";
+    for($j=0;$j<$n;$j++){
+        echo "<td>" . $array[$i][$j] . "</td>";
+    }
+    echo "</tr>";
+}
+echo "</table>";
 
 
